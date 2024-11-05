3dbfccff7513e4a64ad0bdb063b7cf88d4faef05
Sudeep Nair <sudeepn15@gmail.com>
2024-11-05 22:53:04 +0000

    add BusStop class

---
+++ models/busStop.py
@@ -0,0 +1,11 @@
+class BusStop:
+    def __init__(self, stop_id):
+        self.stop_id = stop_id
+        self.waiting_passengers = []
+
+    def generate_passenger_request(self, total_stops):
+        #TODO: Add generate passenger request logic
+
+    def add_passenger(self, passenger):
+        #TODO: Add add passenger logic
+
