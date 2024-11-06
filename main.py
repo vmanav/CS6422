736420595c21916a27c5fd27f5549d3c1d7656a5
Yugansh <yugansh416@gmail.com>
2024-11-06 14:56:18 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 from models.bus import Bus
 from models.busStop import BusStop
+from models.passenger import Passenger
 
 def main():
     b = Bus(capacity=10, current_stop=0)
     busStop = BusStop(1)
+    passenger = Passenger(start_stop=1, end_stop=3)
 
     print("Bus Details")
     print("capacity :", b.capacity)
@@ -14,6 +16,10 @@ def main():
     print("Bus Stop Details")
     print("Stop id:", busStop.stop_id)
     print("Passengers waiting on stop:", busStop.waiting_passengers)
+
+    print("Passenger Details")
+    print("Start Stop:",passenger.start_stop)
+    print("End Stop:",passenger.end_stop)
 
 if __name__ == "__main__":
     main()
